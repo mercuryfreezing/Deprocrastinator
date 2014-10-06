d15be516d9144ee54bb230c426961cf3890fab67
roshan <roshan.prakash.r@gmail.com>
2014-10-06 20:24:43 +0100

    Initial Commit

---
+++ Deprocrastinator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Deprocrastinator
+//
+//  Created by roshan on 06/10/2014.
+//  Copyright (c) 2014 learningIOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Deprocrastinator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Deprocrastinator
+//
+//  Created by roshan on 06/10/2014.
+//  Copyright (c) 2014 learningIOS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
