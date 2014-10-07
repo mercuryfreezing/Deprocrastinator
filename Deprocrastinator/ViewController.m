66cb0177957943dfe510513cbd158aea154b5619
roshan <roshan.prakash.r@gmail.com>
2014-10-07 01:13:53 +0100

    Till task 6

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource    >
+@property NSMutableArray *toDoItems;
+@property (weak, nonatomic) IBOutlet UITextField *toDoTextField;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIButton *editDoneButton;
+
+
 
 @end
 
@@ -16,12 +22,92 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.toDoItems = [NSMutableArray arrayWithObjects: @"Give clothes to laundry",
+                                                @"Get car serviced",
+                                                @"Read up on auto-layouts",
+                                                @"Optimize code",
+                                                @"Go to the gym",
+                                                nil];
+
+    [self.tableView setAllowsSelection:YES];
+    [self.tableView setAllowsMultipleSelection:YES];
+
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+
+    return [self.toDoItems count];
+}
+
+//Task1
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"myCell" forIndexPath:indexPath];
+    UISwipeGestureRecognizer *gesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeToChangePriority:)];
+    gesture.direction = UISwipeGestureRecognizerDirectionRight;
+    [cell.contentView addGestureRecognizer:gesture];
+
+    cell.textLabel.text = [self.toDoItems objectAtIndex:indexPath.row];
+
+       return cell;
+}
+
+//Task 2
+- (IBAction)onAddButtonPressed:(UIButton *)sender {
+
+    NSString *toDoText = self.toDoTextField.text;
+    [self.toDoItems addObject:toDoText];
+    NSLog(@"%@", self.toDoItems);
+    [self.tableView reloadData];
+
+}
+
+//Task3
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    indexPath = indexPath;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"myCell" forIndexPath:indexPath];
+
+
+
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+
+
+    [self.tableView reloadData];
+
+}
+
+//Task4
+- (IBAction)onEditButtonPressed:(UIButton *)sender {
+
+    [self.tableView setEditing:YES];
+    [sender setTitle:@"Done" forState:UIControlStateNormal];
+
+}
+
+//Task 4 - 5
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    [self.toDoItems removeObjectAtIndex:indexPath.row];
+    NSArray *indexPathArray = [NSArray arrayWithObject:indexPath];
+    [self.tableView deleteRowsAtIndexPaths:indexPathArray withRowAnimation:UITableViewRowAnimationAutomatic];
+    [self.tableView setEditing:NO];
+    [self.editDoneButton setTitle:@"Edit" forState:UIControlStateNormal];
+
+}
+
+-(IBAction)swipeToChangePriority:(UISwipeGestureRecognizer *)sender{
+
+
+    if (sender.state == UIGestureRecognizerStateEnded) {
+        CGPoint swipeLocation = [sender locationInView:self.tableView];
+        NSIndexPath *swipedIndexPath = [self.tableView indexPathForRowAtPoint:swipeLocation];
+        UITableViewCell* swipedCell = [self.tableView cellForRowAtIndexPath:swipedIndexPath];
+        swipedCell.textLabel.textColor = [UIColor redColor];
+
+}
+
+
 }
 
 @end
